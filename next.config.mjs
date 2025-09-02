2566cc48720ada14b9fd95a332b02c02f74687e1
Kopcho <rich@kopcho.com>
2025-09-01 21:33:09 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-}
+  images: {
+    unoptimized: true, // ✅ disable optimization for static export
+  },
+  output: 'export', // ✅ tells Next.js to generate /out
+};
 
-export default nextConfig
-
+export default nextConfig;
